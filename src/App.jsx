00adf18c6f48953ebec4134dbe120998c34a876c
Sai Sashreek Dittakavi <saisashreekd@gmail.com>
2026-03-11 18:27:13 +0530

    Content Updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ export default function App() {
 
   return (
     <>
-      <h1>Hello Welcome to Productivity</h1>
+      <h1 className="text-blue-500 font-400 text-2xl text-center">Hello Welcome to Productivity</h1>
     </>
   )
 }
